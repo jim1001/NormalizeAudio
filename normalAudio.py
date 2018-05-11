11697c893339762049b2ad96b194c0145f742801
James Schumm <j.schumm@manchester.ac.uk>
2018-05-11 22:16:05 +0100

    Summary: Deal with different filetypes in same run
    Changes:
    modified:   normalAudio.py
    -- Run appropriate audio encoder for files with extensions:
    -- mp4, webm, m4a (case sensitive)
    
    Tested:
    -- Print to console of encode type for test - normalize not done yet

--- normalAudio.py
+++ normalAudio.py
@@ -1,17 +1,36 @@
 import os
+import subprocess
 
-inDir = r"Z:\Temp\DemPower\AudioNormalize"
-outDir = r"Z:\Temp\DemPower\AudioNormalize\out"
-files = os.listdir(inDir)
+basePath = r"Z:\Temp\DemPower\AudioNormalize\Activities"
 
 #use of * - see Unpacking Argument Lists
 #https://goo.gl/F4LpBS
 #print(*files, sep='\n')
 
-for file in files:
-    inFile = os.path.join(inDir,file)
-    outFile = os.path.join(outDir,file)
-    myCmd =  "ffmpeg-normalize " + inFile + " -c:a aac --normalization-type peak --target-level -1 --output " + outFile
-    print(myCmd + '\n')
+for fname in os.listdir(basePath):
+	inPath = os.path.join(basePath, fname)
+	if os.path.isdir(inPath):
+        # skip directories
+		continue
+	outDir = os.path.join(basePath, "out")
+	outPath = os.path.join(outDir,fname)
+	(dummy, extension) = os.path.splitext(fname)
+	if extension.lower() == ".m4a" or extension.lower() == ".mp4":
+		##File has aac encoded sound
+		encode = "aac"
+		myCmd1 =  "ffmpeg-normalize " + inPath + \
+            " -c:a aac --normalization-type peak --target-level -1 --output " \
+            + outPath
+	elif extension.lower() == ".webm":
+		##File has vorbis encoded sound
+		encode = "vorbis"	
+		myCmd1 =  "ffmpeg-normalize " + inPath + \
+             " -c:a libvorbis --normalization-type peak --target-level -1 --output " \
+             + outPath
+	else:
+		encode = "unknown extension"	
+	print(fname, encode,'\n')
+    #p1 = subprocess.Popen(myCmd1, shell=True)
+    #p1.wait()
 
 
